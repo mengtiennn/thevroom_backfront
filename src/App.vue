de8fe961a61d4729ed3042073631c9a670a16aad
chencheng0924 <abs255667@gmail.com>
2021-07-28 00:04:52 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <router-view></router-view>
+    <h1>123</h1>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
 <style lang="scss">
-  // Import Main styles for this application
-  @import 'assets/scss/style';
+// Import Main styles for this application
+@import "assets/scss/style";
 </style>
